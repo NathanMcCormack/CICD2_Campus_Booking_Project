cec80ff8976980c4236ba3548dc31cd813789d2c
NathanMcCormack <118372005+NathanMcCormack@users.noreply.github.com>
2025-11-07 12:24:29 +0000

    Adding Patch endpoint

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException, status 
+from fastapi import FastAPI, Depends, HTTPException, status, Response 
 from sqlalchemy.orm import Session 
 from sqlalchemy import select 
 from sqlalchemy.exc import IntegrityError 
  
 from .database import engine, SessionLocal 
 from .models import Base, UserDB 
-from .schemas import UserCreate, UserRead 
+from .schemas import UserCreate, UserRead, UserUpdate
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
@@ -28,6 +28,10 @@ def get_db():
 def get_users():
     return {"status": "ok"} 
 
+#------------- Users Endpoints ------------------
+
+# GET: All Users, User by ID
+
 @app.get("/api/users", response_model=list[UserRead]) 
 def list_users(db: Session = Depends(get_db)): 
     stmt = select(UserDB).order_by(UserDB.id) 
@@ -40,6 +44,7 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found") 
     return user 
  
+ #POST new user
 @app.post("/api/users", response_model=UserRead, status_code=status.HTTP_201_CREATED) 
 def add_user(payload: UserCreate, db: Session = Depends(get_db)): 
     user = UserDB(**payload.model_dump()) 
@@ -52,3 +57,21 @@ def add_user(payload: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=409, detail="User already exists") 
     return user 
 
+#PATCH user information
+@app.patch("/api/users/{user_id}", response_model=UserRead)
+def update_user(user_id: int, payload: UserUpdate, db: Session = Depends(get_db)):
+    user = db.get(UserDB, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    updates = payload.model_dump(exclude_unset=True, exclude_none=True) #exclude unset only changes the fields that have been updated 
+    for field, value in updates.items():
+        setattr(user, field, value)
+
+    try:
+        db.commit()
+        db.refresh(user)
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=409, detail="User update failed (unique constraint)")
+    return user
